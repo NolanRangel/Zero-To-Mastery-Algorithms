c5fc4c9cb4e2bd18f8d8e54cc75d44ab09202a88
NolanRangel <90576911+NolanRangel@users.noreply.github.com>
2022-03-23 15:18:39 -0600

    reverse string

---
+++ algorithms/arrays/reverseString.js
@@ -0,0 +1,10 @@
+function reverse(str) {
+    let newStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+
+}
+
+console.log(reverse('Hi my name is Nolan'));
