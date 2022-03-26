064f6eb30aa8af4df0cc17d7009a451013d90999
Nolan Rangel <rangelnolan9@gmail.com>
2022-03-26 13:22:22 -0600

    hashTable keys()

--- algorithms/hashTables/HashTable.js
+++ algorithms/hashTables/HashTable.js
@@ -39,16 +39,40 @@ class HashTable {
         return undefined
     }
 
-    // O(n)
+    // #1 does not check for collisions O(n)
+    // #2 does check for collisions O(n^2)
     keys() {
-        const keysArray = [];
+        if (!this.data.length) {
+            return undefined
+        }
+
+        // #1
+        // const keysArray = [];
+        // for (let i = 0; i < this.data.length; i++) {
+        //     if (this.data[i]) {
+        //         // console.log(this.data[i][0][0]);
+        //         keysArray.push(this.data[i][0][0])
+        //     }
+        // }
+        // return keysArray
+
+        // #2
+        let result = []
+        // loop through all the elements
         for (let i = 0; i < this.data.length; i++) {
-            if (this.data[i]) {
-                // console.log(this.data[i][0][0]);
-                keysArray.push(this.data[i][0][0])
+            // if it's not an empty memory cell
+            if (this.data[i] && this.data[i].length) {
+                // but also loop through all the potential collisions
+                if (this.data.length > 1) {
+                    for (let j = 0; j < this.data[i].length; j++) {
+                        result.push(this.data[i][j][0])
+                    }
+                } else {
+                    result.push(this.data[i][0])
+                }
             }
         }
-        return keysArray
+        return result;
     }
 }
 
