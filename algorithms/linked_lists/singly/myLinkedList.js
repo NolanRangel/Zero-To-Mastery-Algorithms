e12da2bff0e5e1e667bb7d14094e4baf9e1fdcb4
Nolan Rangel <rangelnolan9@gmail.com>
2022-03-27 19:20:57 -0600

    Notes

--- algorithms/linked_lists/singly/myLinkedList.js
+++ algorithms/linked_lists/singly/myLinkedList.js
@@ -17,20 +17,20 @@ class LinkedList {
     }
     append(value) {
         const newNode = new Node(value);
-
+        // the tails next changes from null to the newNode
         this.tail.next = newNode;
+        // the tail is now set to the newNode and next is null
         this.tail = newNode;
         this.length++;
-        // return this
     }
 
     prepend(value) {
         const newNode = new Node(value);
-
+        // gets the current head and the newNode to the old head
         newNode.next = this.head;
+        // the head is now the newNode
         this.head = newNode;
         this.length++;
-        // return this;
     }
 
     insert(index, value) {
@@ -43,12 +43,17 @@ class LinkedList {
         }
         // create new Node
         const newNode = new Node(value);
+
         // grabs the node at the apprpriate index
         const leader = this.traversToIndex(index - 1);
-        // create memory reference of the current node
+
+        // create memory reference of the index requested
+        // index - 1 is grabbed so leader.next grabs the actual requested value
         const holdingPointer = leader.next;
+
         // replace next with newNode
         leader.next = newNode;
+
         // point the newNode to the node it is replacing
         newNode.next = holdingPointer;
         this.length++;
@@ -59,6 +64,7 @@ class LinkedList {
         const leader = this.traversToIndex(index - 1);
         // console.log(leader);
         leader.next = leader.next.next;
+        this.length--;
 
     }
 
